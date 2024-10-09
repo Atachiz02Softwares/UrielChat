f16271a6fd15b795fc38f438ceafc94e67744fc7
Mophe <87334468+Mopheshi@users.noreply.github.com>
2024-10-09 11:56:32 +0100

    Added scroll animation...

--- lib/providers/chat_provider.dart
+++ lib/providers/chat_provider.dart
@@ -3,13 +3,16 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../models/chat_message.dart';
 import '../services/chat_service.dart';
+import 'typing_animation_provider.dart';
 
 class ChatProvider extends StateNotifier<List<ChatMessage>> {
   final ChatService _chatService;
   final String _userId;
   final String _chatId;
+  final StateNotifierProviderRef<ChatProvider, List<ChatMessage>> _ref;
 
-  ChatProvider(this._userId, this._chatId, this._chatService) : super([]) {
+  ChatProvider(this._userId, this._chatId, this._chatService, this._ref)
+      : super([]) {
     _init();
   }
 
@@ -21,6 +24,9 @@ class ChatProvider extends StateNotifier<List<ChatMessage>> {
 
   Future<void> addMessage(ChatMessage message) async {
     await _chatService.saveMessage(_chatId, message);
+    if (message.sender == 'AI') {
+      _ref.read(typingAnimationProvider.notifier).resetAnimation();
+    }
   }
 
   Future<void> clearMessages() async {
@@ -37,7 +43,7 @@ final chatProvider = StateNotifierProvider<ChatProvider, List<ChatMessage>>(
   (ref) {
     final userId = FirebaseAuth.instance.currentUser?.uid ?? '';
     final chatId = generateChatId();
-    return ChatProvider(userId, chatId, ChatService());
+    return ChatProvider(userId, chatId, ChatService(), ref);
   },
 );
 
